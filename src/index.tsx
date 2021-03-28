e2d6af4fb84fffa86488dfdccc61b73216dc641e
임진택 <kococo9504@gmail.com>
2021-03-28 14:03:41 +0900

    Add React Component

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = ()=> {
+    return(
+        <>
+            <div>SnowPack Component</div>
+
+        </>
+    )
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
